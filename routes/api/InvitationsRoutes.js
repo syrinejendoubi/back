d314bd3b7f02bec88a103cd5839bfa35d6b4feb2
MzOumaima <mzoughi.oumaimaa@gmail.com>
2022-02-22 23:17:56 +0100

    CRUD INVITATIONS

---
+++ routes/api/InvitationsRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { findAllInvitation , createInvitation , findInvitation,  deleteInvitation , updateInvitation , } = require("../../controllers/invitationsController");
+const { protect, authorize } = require("../../middleware/authMiddleware");
+// Get All invitations
+router.get("/invitations" ,protect, findAllInvitation);
+// Add invitation
+router.post("/invitations", createInvitation);
+// get invitation by id 
+router.get("/invitations/:invitationId" , findInvitation );
+// update invitation
+router.put("/invitations/:invitationId" ,updateInvitation );
+// delete invitation
+router.delete("/invitations/:invitationId" ,deleteInvitation );
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const auth = require("./routes/api/authRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 const users = require("./routes/api/usersRoutes");
 const discipline = require("./routes/api/disciplinesRoutes");
+const invitations = require("./routes/api/invitationsRoutes");
 
 const swaggerDocument = YAML.load("./swagger.yaml");
 
@@ -25,7 +26,8 @@ ConnectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use("/api", auth , users , discipline);
+
+app.use("/api", auth , users , discipline,invitations);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler);
 app.all("*", (req, res) => {
