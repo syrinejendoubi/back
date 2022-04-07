81e8ab8afc49e1359ae811e0bc36a3bc662b4c94
MzOumaima <mzoughi.oumaimaa@gmail.com>
2022-04-07 02:35:11 +0100

    event routes

---
+++ routes/api/eventRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { findAllEvent , createEvent , findEvent,  deleteEvent , updateEvent , } = require("../../controllers/eventController");
+const { protect, authorize } = require("../../middleware/authMiddleware");
+// Get All events
+router.get("/events" , findAllEvent);
+// Add event
+router.post("/events", createEvent);
+// get event by id 
+router.get("/events/:eventId" , findEvent );
+// update event
+router.put("/events/:eventId" ,updateEvent );
+// delete event
+router.delete("/events/:eventId" ,deleteEvent );
+
+
+module.exports = router;
