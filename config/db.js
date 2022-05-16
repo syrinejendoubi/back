4ffbb5df7b9067ac7ff7301ee807e225704352a6
Hassene Ayoub <hassene.ayoub@yahoo.fr>
2022-05-17 01:32:49 +0200

    test discipline (not completed)

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const ConnectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(
+      "mongodb+srv://ReactProject:ReactProject@cluster0.aaffy.mongodb.net/Hicotech?retryWrites=true&w=majority"
+    )
     .then(() => console.log("connected to mongoDB"))
     .catch((err) => {
       console.error(err);
